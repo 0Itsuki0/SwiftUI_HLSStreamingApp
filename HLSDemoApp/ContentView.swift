4b38ab421a5e4e9b046662b19d15efee8d10239e
itsuki <itsuki.enjyoy@gmail.com>
2025-09-20 14:35:23 +0900

    Initial Commit

---
+++ HLSDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HLSDemoApp
+//
+//  Created by Itsuki on 2025/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HLSDemoApp/HLSDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HLSDemoAppApp.swift
+//  HLSDemoApp
+//
+//  Created by Itsuki on 2025/09/20.
+//
+
+import SwiftUI
+
+@main
+struct HLSDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
